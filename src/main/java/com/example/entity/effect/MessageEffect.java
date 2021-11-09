8f2a140442e1e11914df1535797a72fe000599e8
Moonpearl <romain.tello@gmail.com>
2021-11-09 16:00:28 +0100

    Add end game effects

--- src/main/java/com/example/entity/effect/MessageEffect.java
+++ src/main/java/com/example/entity/effect/MessageEffect.java
@@ -1,25 +1,30 @@
 package com.example.entity.effect;
 
 /**
- * Effet permettant d'afficher un message dans la console
+ * Représente un effet permettant d'afficher un message dans la console
  */
-public class MessageEffect
+public class MessageEffect implements Effect
 {
     /**
      * Le message à afficher
      */
     private String message;
 
+    /**
+     * Crée un nouvel effet
+     * @param message Le message à afficher
+     */
     public MessageEffect(String message)
     {
         this.message = message;        
     }
 
     /**
-     * Affiche le message
+     * Déclenche l'effet
      */
-    public void display()
+    public void trigger()
     {
+        // Affiche le message dans la console
         System.out.println(message);
     }
 }
